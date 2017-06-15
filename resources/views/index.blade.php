e04159d3eb800a3e00d37ffd6c8b3f14d8a9d29a
curo suke <corosuke2k@gmail.com>
2017-06-15 22:22:14 +0900

    ・giganticスキーマ読み込み ・メニュー名を追加＋変更 ・サーバステータス欄と広告スペースを追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,25 +7,34 @@
         <!--タブのボタン部分-->
         <ul class="nav nav-tabs">
             <li class="nav-item active">
-                <a href="#tab1" class="nav-link bg-primary" data-toggle="tab">整地量ランキング</a>
+                <a href="#tab1" class="nav-link bg-primary" data-toggle="tab">総　合</a>
             </li>
             <li class="nav-item">
-                <a href="#tab2" class="nav-link bg-primary" data-toggle="tab">建築量ランキング</a>
+                <a href="#tab2" class="nav-link bg-primary" data-toggle="tab">整 地 量</a>
             </li>
             <li class="nav-item">
-                <a href="#tab2" class="nav-link bg-primary" data-toggle="tab">接続時間ランキング</a>
+                <a href="#tab3" class="nav-link bg-primary" data-toggle="tab">建 築 量</a>
+            </li>
+            <li class="nav-item">
+                <a href="#tab4" class="nav-link bg-primary" data-toggle="tab">接続時間</a>
             </li>
             @if ($navbar_act !== 'daily')
                 <li class="nav-item">
-                    <a href="#tab3" class="nav-link bg-primary" data-toggle="tab">投票数ランキング</a>
+                    <a href="#tab3" class="nav-link bg-primary" data-toggle="tab">投 票 数</a>
                 </li>
             @endif
         </ul>
         <!--タブのコンテンツ部分-->
         <div class="tab-content">
             <div id="tab1" class="tab-pane active">
-                <h3>◇ 整地量ランキング</h3>
+                <h3>◇ 総合ランキング</h3>
+                <div class="rank">
+                    ※ 近日公開予定
+                </div>
+            </div>
 
+            <div id="tab2" class="tab-pane">
+                <h3>◇ 整地量ランキング</h3>
 
                 {{--<div class="fixing-base">--}}
                 {{--<div class="fixing-box">--}}
@@ -49,15 +58,15 @@
                             @foreach ($ranking_data as $key => $item)
                                 <tr>
                                     <th scope="row">
-                                        {{$item->rank}}位
+                                        <big>{{$item->rank}}位</big>
                                     </th>
                                     <td>
                                         <img src="{{$item->mob_head_img}}">
                                     </td>
                                     <td>
                                         {{ $item->name }}<br>
-                                        <span class="num_break">総整地量：{{ number_format($item->totalbreaknum) }}</span><br>
-                                        <span class="last_login">Last loign: {{$item->lastquit}}</span>
+                                        <span class="num_break">総整地量：{{ number_format($item->allmineblock) }}</span><br>
+                                        {{--<span class="last_login">Last loign: {{$item->lastquit}}</span>--}}
                                     </td>
                                 </tr>
                             @endforeach
@@ -72,13 +81,13 @@
                 </div>
             </div>
 
-            <div id="tab2" class="tab-pane">
+            <div id="tab3" class="tab-pane">
                 <h3>◇ 建築量ランキング</h3>
                 <div class="rank">
                     ※ 近日公開予定
                 </div>
             </div>
-            <div id="tab2" class="tab-pane">
+            <div id="tab4" class="tab-pane">
                 <h3>◇ 接続時間ランキング</h3>
                 <div class="rank">
                     ※ 近日公開予定
@@ -86,7 +95,7 @@
             </div>
             {{-- 累計のみ表示する --}}
             @if ($navbar_act !== 'daily')
-                <div id="tab3" class="tab-pane">
+                <div id="tab5" class="tab-pane">
                     <h3>◇ 投票数ランキング</h3>
                     <div class="rank">
                         ※ 近日公開予定
