c2ff8fb36826df3be1534fac29781d163f5d926f
curosuke <corosuke2k@gmail.com>
2017-07-09 00:49:16 +0900

    JMSログイン

---
+++ app/SocialAccountService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Laravel\Socialite\Contracts\User as ProviderUser;
+
+class SocialAccountService
+{
+    public function findOrCreate(ProviderUser $providerUser, $provider)
+    {
+        $account = LinkedSocialAccount::where('provider_name', $provider)
+            ->where('provider_id', $providerUser->getId())
+            ->first();
+
+        if ($account) {
+            return $account->user;
+        } else {
+
+            $user = User::where('email', $providerUser->getEmail())->first();
+
+            if (! $user) {
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name'  => $providerUser->getName(),
+                ]);
+            }
+
+            $user->accounts()->create([
+                'provider_id'   => $providerUser->getId(),
+                'provider_name' => $provider,
+            ]);
+
+            return $user;
+
+        }
+    }
+}
