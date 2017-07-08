5d96ef5eefef3b7478d934fca655d40102d463d9
nakano_shingo <corosuke2y@gmail.com>
2017-07-09 04:38:16 +0900

    favicon設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 </head>
 <body>
     <div id="app">
