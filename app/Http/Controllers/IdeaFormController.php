05ef51c82a5ab9af348d3f90ef1d6d6461107f87
curo suke <corosuke2k@gmail.com>
2017-07-09 04:07:29 +0900

    エラー用ページの整備 アイディアの文言を統一

--- app/Http/Controllers/IdeaFormController.php
+++ app/Http/Controllers/IdeaFormController.php
@@ -58,10 +58,10 @@ class IdeaFormController extends Controller
         // クッキーが生きて入れば
         if (!empty($_COOKIE["count"])) {
             Log::debug('$count -> '.print_r($_COOKIE["count"], 1));
-            return redirect('ideaForm')->with('message', "アイディアの連続投稿はご遠慮ください。\nしばらく時間を置いてから投稿をお願いします。");
+            return redirect('ideaForm')->with('message', "アイデアの連続投稿はご遠慮ください。\nしばらく時間を置いてから投稿をお願いします。");
         }
         else {
-            //
+            // クッキーをクライアント(ブラウザ)へ保存する
             $cookie = \Cookie::make('count', md5(uniqid(mt_rand(), true)), 1);
             return Response::view('ideaSubmitted')->withCookie($cookie);
         }
