801a7222f26c7661526182d6dc8e88bfe48a8105
nakano_shingo <corosuke2y@gmail.com>
2017-07-31 00:34:55 +0900

    微修正

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -9,8 +9,7 @@
         <li>
             本ランキングページに関するお問い合わせは、
             <a href="http://seichi.click/d/Discord">Discord</a>もしくは
-            <a href="https://twitter.com/corosuke9">Twitter</a>から<br>
-            「curosuke」宛にご連絡ください。
+            <a href="https://twitter.com/corosuke9">Twitter</a>から「curosuke」宛にご連絡ください。
         </li>
         {{--<li><a href="https://minecraft.jp/players/75f6e1bc1a064470a67b2654ea4bf0c2">JMS</a>--}}
         {{--<li><a href="">整地鯖Redmineプロジェクト：ユーザページ</a></li>--}}
