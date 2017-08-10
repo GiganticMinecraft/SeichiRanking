a4764d712d96814df03e4924ffeec909cdb47f54
curosuke <corosuke2k@gmail.com>
2017-08-10 23:47:10 +0900

    接続時間ランキングを実装

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,11 +15,11 @@
             <li class="nav-item @if (app('request')->input('kind') == 'build')active @endif">
                 <a href="#tab3" class="nav-link bg-primary" data-toggle="tab">建 築 量</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item @if (app('request')->input('kind') == 'playtime')active @endif">
                 <a href="#tab4" class="nav-link bg-primary" data-toggle="tab">接続時間</a>
             </li>
             @if ($navbar_act !== 'daily')
-                <li class="nav-item">
+                <li class="nav-item @if (app('request')->input('kind') == 'vote')active @endif">
                     <a href="#tab5" class="nav-link bg-primary" data-toggle="tab">投 票 数</a>
                 </li>
             @endif
@@ -111,7 +111,36 @@
             <div id="tab4" class="tab-pane">
                 <h3>◇ 接続時間ランキング</h3>
                 <div class="rank">
-                    ※ 近日公開予定
+                    @if (!empty($navbar_act) && $navbar_act == 'year' || $navbar_act == 'monthly' || $navbar_act == 'weekly' || $navbar_act == 'daily')
+                        ※ 近日公開予定
+                    @else
+                        <table class="table table-striped table-hover">
+                            <tbody>
+
+                            @foreach ($playtime_ranking as $key => $item)
+                                <tr>
+                                    <th scope="row">
+                                        <big>{{$item->rank}}位</big>
+                                    </th>
+                                    <td>
+                                        <img src="{{$item->mob_head_img}}">
+                                    </td>
+                                    <td>
+                                        {{ $item->name }}<br>
+                                        {{--<span class="num_break">総整地量：{{ number_format($item->allmineblock) }}</span><br>--}}
+                                        <span class="num_break">接続時間：{{ $item->playtime }}</span><br>
+                                        <span class="last_login">Last loign: {{$item->lastquit}}</span>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+
+                        {{-- ページネーション --}}
+                        {!! $playtime_ranking->appends(['kind' => 'playtime'])->links() !!}
+
+                    @endif
+
                 </div>
             </div>
             {{-- 累計のみ表示する --}}
