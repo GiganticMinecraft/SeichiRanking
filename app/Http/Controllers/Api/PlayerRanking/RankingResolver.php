3673b64aff8eb3eb68262b6c11e15d7d6a89de63
Kory33 <6561358+kory33@users.noreply.github.com>
2017-09-02 00:36:34 +0900

    /api/ranking/player/{playername} エンドポイントの実装

--- app/Http/Controllers/Api/PlayerRanking/RankingResolver.php
+++ app/Http/Controllers/Api/PlayerRanking/RankingResolver.php
@@ -10,12 +10,12 @@ abstract class RankingResolver
 
     abstract function getRankingType();
 
-    private function toPlayerRank($player)
+    private function toPlayerRank($ranked_player)
     {
-        $player_rank = $player->rank;
-        unset($player->rank);
+        $player_rank = $ranked_player->rank;
+        unset($ranked_player->rank);
         return [
-            "player" => $player,
+            "player" => $ranked_player,
             "type" => $this->getRankingType(),
             "rank" => $player_rank
         ];
@@ -44,5 +44,22 @@ abstract class RankingResolver
         }
 
         return array_slice($ranked_players, $offset - 1, $limit);
+    }
+
+    public function getPlayerRank($player_name)
+    {
+        $comparator = $this->getRankComparator();
+
+        // TODO this query cannot be cached, but instead a (name => rank) map can be generated from all-players-ranking
+        $ranked_player = DB::table('playerdata as t1')
+            ->select(
+                'name',
+                'uuid',
+                DB::raw('(select count(*)+1 from playerdata as t2 where t2.' . $comparator . ' > t1.' . $comparator . ') as rank')
+            )
+            ->where('name', $player_name)
+            ->first();
+
+        return $this->toPlayerRank($ranked_player);
     }
 }
