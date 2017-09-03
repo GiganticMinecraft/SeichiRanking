9bf66a9487f643bdc340949b383dfa4279911d2e
curosuke <corosuke2k@gmail.com>
2017-09-04 00:07:16 +0900

    adsence

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,6 +48,14 @@
         ga('send', 'pageview');
 
     </script>
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-1577125384876056",
+            enable_page_level_ads: true
+        });
+    </script>
 </head>
 <body>
     {{-- ナビゲーションバーの Partial を使用 --}}
