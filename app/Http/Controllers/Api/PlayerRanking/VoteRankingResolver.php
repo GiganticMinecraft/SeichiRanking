bbae785ea1a66510e923f9328db4d69944827b35
curo suke <corosuke2k@gmail.com>
2017-09-06 11:37:27 +0900

    /api/rankingのvoteランキングの応答のtypeプロパティを修正

--- app/Http/Controllers/Api/PlayerRanking/VoteRankingResolver.php
+++ app/Http/Controllers/Api/PlayerRanking/VoteRankingResolver.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers\Api\PlayerRanking;
 class VoteRankingResolver extends RankingResolver
 {
     const COMPARE_TARGET = 'p_vote';
-    const RANKING_TYPE = 'break';
+    const RANKING_TYPE = 'vote';
 
     function getRankComparator()
     {
