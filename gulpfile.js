56d37cabdae55baff0bf0388bc4a1eb049673ec0
Kory33 <6561358+kory33@users.noreply.github.com>
2017-09-13 20:13:56 +0900

    APIクラスを実装

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const elixir = require('laravel-elixir');
 
 elixir(mix => {
-    mix.browserify('resources/assets/jsx/ranking.jsx', 'public/js/jsx');
+    mix.browserify('resources/assets/jsx/ranking.jsx', 'public/js/jsx', 'resources/assets');
 });
---
+++ resources/assets/js/ranking-api.js
@@ -0,0 +1,17 @@
+import fetch from 'isomorphic-fetch';
+
+const endpoint_root = "/api";
+
+export default class RankingApi {
+    static searchPlayer(query, lim = 20) {
+        return fetch(`${endpoint_root}/search/player?q=${query}&lim=${lim}`);
+    }
+
+    static getRanking(type = "break", offset = 1, lim = 100) {
+        return fetch(`${endpoint_root}/ranking?type=${type}&offset=${ofset}&lim=${lim}`);
+    }
+
+    static getPlayerRanking(player_uuid, types="break,build,playtime,vote") {
+        return fetch(`${endpoint_root}/ranking/player/${player_uuid}&types=${types}`)
+    }
+}
