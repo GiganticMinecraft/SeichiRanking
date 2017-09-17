5ddd8762298a77ec08190266b5f9dc50eaccfaf8
nakano_shingo <corosuke2y@gmail.com>
2017-09-17 15:08:33 +0900

    navbarレイアウトを移植してみた

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,35 +1,39 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
 
-    <h3>このページについて</h3>
+    <div class="container">
 
-    <br>
+        <h3>このページについて</h3>
 
-    <ul>
-        <li>
-            本サイトは、以下のMinecraft PC版 マルチサーバの「ギガンティック☆整地鯖」における、各種プレイヤーのランキングを掲載しています。
-        </li>
-    </ul>
-
-    <hr>
-
-    <h3>推奨ブラウザ</h3>
-
-    <br>
-
-    <ul>
-
-        <li>
-            本サイトでは、以下のブラウザを推奨しております。記載以外のブラウザ(Internet Explorer等)での動作は保障致しません。
-        </li>
+        <br>
 
         <ul>
-            <li>Google Chrome</li>
-            <li>Firefox</li>
-            <li>Safari</li>
+            <li>
+                本サイトは、以下のMinecraft PC版 マルチサーバの「ギガンティック☆整地鯖」における、各種プレイヤーのランキングを掲載しています。
+            </li>
         </ul>
 
-    </ul>
+        <hr>
+
+        <h3>推奨ブラウザ</h3>
+
+        <br>
+
+        <ul>
+
+            <li>
+                本サイトでは、以下のブラウザを推奨しております。記載以外のブラウザ(Internet Explorer等)での動作は保障致しません。
+            </li>
+
+            <ul>
+                <li>Google Chrome</li>
+                <li>Firefox</li>
+                <li>Safari</li>
+            </ul>
+
+        </ul>
+
+    </div>
 
 @endsection
