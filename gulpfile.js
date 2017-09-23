2c6d458fb8f460f59cb57c05de2496c203ba41e1
Kory33 <6561358+kory33@users.noreply.github.com>
2017-09-23 17:30:18 +0900

    es2016の機能を使えるようにpackage.json諸々を更新する

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
+/*
 const elixir = require('laravel-elixir');
 
 elixir(mix => {
     mix.browserify('resources/assets/jsx/ranking.jsx', 'public/js/jsx', 'resources/assets');
 });
+*/
+
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+
+gulp.task('browserify', () => {
+    browserify('./resources/assets/jsx/ranking.jsx', { debug : false })
+        .transform(babelify).bundle()
+        .on('error', console.log)
+        .pipe(source("ranking.js"))
+        .pipe(gulp.dest('./public/js/jsx'));
+});
+
+gulp.task("default", ['browserify']);
