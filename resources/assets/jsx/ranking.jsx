dccf0535d0d927a40aafda6beb3b0d011420c590
Kory33 <6561358+kory33@users.noreply.github.com>
2017-10-09 02:30:19 +0900

    ページ更新時の動作(アンカー更新/スクロールリセット)を追加する

--- resources/assets/jsx/ranking.jsx
+++ resources/assets/jsx/ranking.jsx
@@ -7,7 +7,28 @@ import RankingBody from "./components/ranking-body.jsx";
 import RankingTypeNavigator from "./components/ranking-type-navigator.jsx"
 import RankingDurationNavigator from "./components/ranking-duration-navigator.jsx";
 import rankingStore from "../js/ranking-store";
+import {observe} from "../../../node_modules/mobx/lib/mobx";
 
 ReactDOM.render(<RankingBody store={rankingStore}/>, document.getElementById('ranking-container'));
 ReactDOM.render(<RankingTypeNavigator/>, document.getElementById('ranking-type-nav'));
 ReactDOM.render(<RankingDurationNavigator/>, document.getElementById('ranking-duration-nav'));
+
+//---------------------------
+// ストアに対するリスナを追加
+
+const toAnchorString = (obj) => "#" + Object.keys(obj)
+    .map(key => [key, obj[key]])
+    .map(([key, val]) => `${key}=${val}`)
+    .join("&");
+
+observe(rankingStore, "ranking", _ => {
+    // アンカーを変更する
+    window.location.hash = toAnchorString({
+        page : rankingStore.page,
+        type : rankingStore.type,
+        duration : rankingStore.duration
+    });
+
+    // ページ変更時にスクロールを巻き戻す
+    window.scrollTo(0, 0);
+});
