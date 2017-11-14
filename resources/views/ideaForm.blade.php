e731567305cba19bf868388acd389677eb7c3584
curo suke <curosuke2k@gmail.com>
2017-11-14 22:42:09 +0900

    adsense導入

--- resources/views/ideaForm.blade.php
+++ resources/views/ideaForm.blade.php
@@ -41,6 +41,17 @@
     </form>
 </div>
 
+{{-- 広告欄 --}}
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1577125384876056"
+     data-ad-slot="4448153429"
+     data-ad-format="auto"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 @include('footer')
 
 @endsection
