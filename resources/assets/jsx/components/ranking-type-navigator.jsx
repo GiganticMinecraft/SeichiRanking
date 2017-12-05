f52911f6f22223e224561e2726559676ac373119
Kory33 <6561358+kory33@users.noreply.github.com>
2017-12-05 19:04:46 +0900

    ranking-type-navigatorを整理

--- resources/assets/jsx/components/ranking-type-navigator.jsx
+++ resources/assets/jsx/components/ranking-type-navigator.jsx
@@ -3,21 +3,16 @@ import RankingTypes from '../../js/ranking-types';
 import rankingStore from '../../js/ranking-store'
 import {observer} from "mobx-react";
 
-@observer
-export default class RankingTypeNavigator extends Component {
-    constructor(props) {
-        super(props);
+class RankingTypeTab extends Component {
+    render() {
+        const tab_type = this.props.type;
 
-        this._getTab = this._getTab.bind(this);
-    }
-
-    _getTab(type) {
-        let item_class_name = "clickable nav-item ranking-type-item";
-        if (rankingStore.type === type) {
-            item_class_name += " active";
+        let tab_classes = "clickable nav-item ranking-type-item";
+        if (rankingStore.type === tab_type) {
+            tab_classes += " active";
         }
 
-        let tab_title = RankingTypes.resolveRaw(type);
+        let tab_title = RankingTypes.resolveRaw(tab_type);
 
         // 長さが不足していれば空白を挿入する
         if (tab_title.length < 4) {
@@ -25,16 +20,21 @@ export default class RankingTypeNavigator extends Component {
         }
 
         return (
-            <li className={item_class_name} key={type}>
-                <a className="nav-link bg-primary" data-toggle="tab" onClick={ () => rankingStore.setType(type) }>{tab_title}</a>
+            <li className={tab_classes}>
+                <a className="nav-link bg-primary" data-toggle="tab" onClick={ () => rankingStore.setType(tab_type) }>{tab_title}</a>
             </li>
         );
     }
+}
 
+@observer
+export default class RankingTypeNavigator extends Component {
     render() {
         return (
             <ul className="nav nav-tabs">
-                { RankingTypes.getAvailableTypes(rankingStore.duration).map(this._getTab) }
+                { RankingTypes.getAvailableTypes(rankingStore.duration).map(
+                    type => <RankingTypeTab type={type} key={type}/>
+                ) }
             </ul>
         );
     }
