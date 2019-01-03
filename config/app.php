f08f4437255b6c7598ce93d0608fa50229fce607
curo suke <corosuke2k@gmail.com>
2019-01-03 10:05:37 +0900

    IDEヘルパ追加

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         /*
          * Application Service Providers...
          */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
