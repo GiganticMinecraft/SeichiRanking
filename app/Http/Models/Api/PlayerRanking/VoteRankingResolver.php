de6ff66f539f08390cddb2d86c2f4bcc29da6bcf
curosuke <corosuke2y@gmail.com>
2019-01-08 00:28:03 +0900

    API＋フロント処理の仮実装

--- app/Http/Models/Api/PlayerRanking/VoteRankingResolver.php
+++ app/Http/Models/Api/PlayerRanking/VoteRankingResolver.php
@@ -5,11 +5,44 @@ namespace App\Http\Models\Api\PlayerRanking;
 class VoteRankingResolver extends RankingResolver
 {
     const COMPARE_TARGET = 'p_vote';
+
+    const TOTAL_TABLE_TARGET = 'playerdata';
+    const TOTAL_COMPARE_TARGET = 'p_vote';
+
+    const DAILY_TABLE_TARGET = 'daily_ranking_table';
+    const DAILY_COMPARE_TARGET = 'vote_count';
+
     const RANKING_TYPE = 'vote';
 
+    /**
+     * ランキングデータを取得するために利用するテーブル名を返却する
+     * @return string
+     */
+    function getRankTable()
+    {
+        if (request('duration') === 'daily') {
+            // デイリー
+            return self::DAILY_TABLE_TARGET;
+        } else {
+            // 総合
+            return self::TOTAL_TABLE_TARGET;
+        }
+    }
+
+    /**
+     * ランキングデータを取得するために利用するカラム名を返却する
+     * @return string
+     */
     function getRankComparator()
     {
-        return self::COMPARE_TARGET;
+        if (request('duration') === 'daily') {
+            // デイリー
+            return self::DAILY_COMPARE_TARGET;
+        } else {
+            // 総合
+            return self::TOTAL_COMPARE_TARGET;
+        }
+
     }
 
     function getRankingType()
