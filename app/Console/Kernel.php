62ec166737c9ecdad0bddc412cfb2b710c9a714a
yokoro13 <yokoro1219@yahoo.co.jp>
2020-07-05 21:50:55 +0900

    UPDATE 重複した処理をまとめた

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\CountRanking::class,
+        Commands\CountRankingCommand::class,
     ];
 
     /**
