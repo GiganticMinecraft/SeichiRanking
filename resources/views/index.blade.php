b4634ea21f2971ed323d425dc58a2c0c5a8033d3
Kisaragi <kisaragi.effective@gmail.com>
2022-01-15 17:39:00 +0900

    remove: server status

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,44 +72,6 @@
         @endif
 
         <div class="row">
-            <div class="col-sm-2 top70">
-                <h4>サーバー稼働状況</h4>
-
-                <p>☆ 合計接続人数：{{$server_status[0]['online'] or 0}}人</p>
-                <table class="table table-responsive">
-                    <tr>
-                        <td class="warning">ネットワークロビー</td>
-                        <td class="warning text-right">{{$server_status[0]['lobby'] or 0}}人</td>
-                    <tr>
-                        <td class="success">アルカディア</td>
-                        <td class="success text-right">{{$server_status[0]['s1'] or 0}}人</td>
-                    </tr>
-                    <tr>
-                        <td class="success">エデン</td>
-                        <td class="success text-right">{{$server_status[0]['s2'] or 0}}人</td>
-                    </tr>
-                    <tr>
-                        <td class="success">ヴァルハラ</td>
-                        <td class="success text-right">{{$server_status[0]['s3'] or 0}}人</td>
-                    </tr>
-                    <tr>
-                        <td class="danger">整地専用</td>
-                        <td class="danger text-right">{{$server_status[0]['s5'] or 0}}人</td>
-                    </tr>
-                    <tr>
-                        <td class="info">公共施設</td>
-                        <td class="info text-right">{{$server_status[0]['s7'] or 0}}人</td>
-                    </tr>
-                    <tr>
-                        <td class="active">SPADE</td>
-                        <td class="active text-right">{{$server_status[0]['spade'] or 0}}人</td>
-                    </tr>
-                    <tr>
-                        <td class="active">整地鯖(春)</td>
-                        <td class="active text-right">{{$server_status[0]['h1'] or 0}}人</td>
-                    </tr>
-                </table>
-            </div>
             <div class="col-sm-8 top70">
                 {{-- メインコンテンツ --}}
                 <div class="m-t-1 m-l-1 m-r-1">
