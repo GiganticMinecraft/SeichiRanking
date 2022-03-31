ddad41159d6c971b05403d9087ad683bc1b403e2
inductor <kela@inductor.me>
2022-03-31 16:59:01 +0900

    comment out

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,16 +15,16 @@ class Controller extends BaseController
      * サーバーステータス(接続人数)を取得する
      * @return mixed|null
      */
-    public function get_server_status()
-    {
-        $server_status = null;
+    // public function get_server_status()
+    // {
+    //     $server_status = null;
 
-        if ($json = @file_get_contents(env('SERVER_STATUS_URL'))) {
-            $server_status = json_decode($json, true);
-//            Log::debug(print_r($server_status, 1));
-        }
+    //     if ($json = @file_get_contents(env('SERVER_STATUS_URL'))) {
+    //         $server_status = json_decode($json, true);
+    //        Log::debug(print_r($server_status, 1));
+    //     }
 
-        return $server_status;
-    }
+    //     return $server_status;
+    // }
 
 }
